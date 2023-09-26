94159152796e3284272a45b8d55efa0489f5559b
Zaphiruz <Jacobthomas428@gmail.com>
2023-09-26 08:33:19 -0700

    resolve relationships

--- src/schemas/recipes.schema.mjs
+++ src/schemas/recipes.schema.mjs
@@ -1,12 +1,22 @@
 import { Schema, model as Model } from 'mongoose';
+import autoPopulatePlugin from 'mongoose-autopopulate';
 
 export const structure = {
 	_id: Schema.Types.ObjectId,
-	item: Schema.Types.ObjectId,
-	materials: [Schema.Types.ObjectId],
+	item: {
+		type: Schema.Types.ObjectId,
+		ref: 'item',
+		autopopulate: true
+	},
+	materials: [{
+		type: Schema.Types.ObjectId,
+		ref: 'item',
+		autopopulate: true
+	}],
 	tags: [String]
 }
 
 export const schema = Schema(structure);
+schema.plugin(autoPopulatePlugin);
 
 export const model = Model('recipe', schema);
